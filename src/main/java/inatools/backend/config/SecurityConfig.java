8f2f61ac4708d7caaf16465ecdfc15610ea7d940
woogie01 <kwonhu3973@gmail.com>
2024-07-26 20:03:49 +0900

    feat: Swagger 세팅 추가

--- src/main/java/inatools/backend/config/SecurityConfig.java
+++ src/main/java/inatools/backend/config/SecurityConfig.java
@@ -2,32 +2,64 @@ package inatools.backend.config;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
+import inatools.backend.jwt.LoginFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-@ConfigurationPropertiesScan
+// @ConfigurationPropertiesScan
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .headers(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .rememberMe(AbstractHttpConfigurer::disable)
-                .logout(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS));
+    private final AuthenticationConfiguration authenticationConfiguration;
 
-        return httpSecurity.build();
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+        http
+                .formLogin(AbstractHttpConfigurer::disable);
+        http
+                .headers(AbstractHttpConfigurer::disable);
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
+        http
+                .rememberMe(AbstractHttpConfigurer::disable);
+        http
+                .logout(AbstractHttpConfigurer::disable);
+        http
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS));
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/signup").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/**", "/api/**").permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
     }
 }
