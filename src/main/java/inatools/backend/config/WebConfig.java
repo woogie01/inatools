1b4a841ad429f0d634b756a88c13e0a7912a42ac
woogie01 <kwonhu3973@gmail.com>
2024-07-29 15:00:31 +0900

    feat : JWT 인증 관련 설정 파일 추가

---
+++ src/main/java/inatools/backend/config/WebConfig.java
@@ -0,0 +1,25 @@
+package inatools.backend.config;
+
+
+import inatools.backend.auth.jwt.util.JwtTokenProvider;
+import inatools.backend.common.annotation.ExtractPayloadArgumentResolver;
+import inatools.backend.common.annotation.ExtractTokenArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new ExtractTokenArgumentResolver(jwtTokenProvider));
+        resolvers.add(new ExtractPayloadArgumentResolver(jwtTokenProvider));
+    }
+}
