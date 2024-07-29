26258636447a798433792a75691397ab04ac9b75
woogie01 <kwonhu3973@gmail.com>
2024-07-29 15:07:53 +0900

    fix : 권한정보 수정하면서 Role 가져오지 못하는 버그 수정

--- src/main/java/inatools/backend/auth/security/domain/CustomUserDetails.java
+++ src/main/java/inatools/backend/auth/security/domain/CustomUserDetails.java
@@ -1,6 +1,8 @@
-package inatools.backend.dto;
+package inatools.backend.auth.security.domain;
 
+import inatools.backend.auth.security.dto.UserDetailsDto;
 import inatools.backend.domain.Member;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -10,26 +12,34 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+@Getter
 @RequiredArgsConstructor
 public class CustomUserDetails implements UserDetails {
 
-    private final Member member;
+    private final UserDetailsDto member;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority(member.getRole()));
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        addRoles(authorities);
         return authorities;
     }
 
-    @Override
-    public String getPassword() {
-        return member.getPassword();
+    private void addRoles(Collection<GrantedAuthority> authorities) {
+        member.getRole()
+                .stream()
+                .map(SimpleGrantedAuthority::new)
+                .forEach(authorities::add);
     }
 
     @Override
-    public String getUsername() {
-        return member.getUsername();
+    public String getUsername() { // 아이디 검증을 위해 받아오기
+        return member.getUserId();
+    }
+
+    @Override
+    public String getPassword() { // 비밀번호 검증을 위해 받아오기
+        return member.getPassword();
     }
 
     @Override
