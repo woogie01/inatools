ec390ccabf176282cd9f031f26c95ee3501a322a
woogie01 <kwonhu3973@gmail.com>
2024-08-01 16:31:08 +0900

    form : 인증/인가 관련 주석 추가

--- src/main/java/inatools/backend/common/annotation/ExtractPayloadArgumentResolver.java
+++ src/main/java/inatools/backend/common/annotation/ExtractPayloadArgumentResolver.java
@@ -19,19 +19,26 @@ public class ExtractPayloadArgumentResolver implements HandlerMethodArgumentReso
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
+
+        // 매개변수가 @ExtractPayload 애노테이션을 가지고 있는지 확인
         return parameter.hasParameterAnnotation(ExtractPayload.class);
     }
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+
+        // 1. NativeWebRequest에서 HttpServletRequest를 추출
         HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
 
+        // 2. HTTP 요청 헤더에서 JWT 토큰을 추출
         String token = AuthorizationExtractor.extractToken(request)
                 .orElseThrow(() -> BaseException.type(GlobalErrorCode.INVALID_PERMISSION));
 
         if (token.isBlank() || token.isEmpty())
             throw BaseException.type(GlobalErrorCode.INVALID_PERMISSION);
 
+
+        // 3. 유효한 토큰에서 페이로드(사용자 ID)를 추출하여 반환
         validateTokenIntegrity(token);
 
         return jwtTokenProvider.getId(token);
