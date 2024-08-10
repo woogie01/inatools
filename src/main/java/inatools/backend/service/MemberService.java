851be364510d4ab0c6b288658f1d73cc436a3a9b
Minkyu Shin <digi1k2001@gmail.com>
2024-08-11 01:43:18 +0900

    Feat: 회원 관련 정보 업데이터 전 검증 로직 추가

--- src/main/java/inatools/backend/service/MemberService.java
+++ src/main/java/inatools/backend/service/MemberService.java
@@ -18,27 +18,30 @@ public class MemberService {
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public Member updateMember(Long id, UpdateMemberRequest updateRequest) {
+    public Member updateMember(String loginId, Long id, UpdateMemberRequest updateRequest) {
         Member member = memberRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 회원이 존재하지 않습니다."));
+        Member.checkMember(loginId, member);
         member.updateMemberInfo(updateRequest, passwordEncoder);
 //        memberRepository.save(member);
         return member;
     }
 
-    public Member getMember(Long id) {
-        return memberRepository.findById(id)
+    public Member getMember(String loginId, Long id) {
+        Member member = memberRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 회원이 존재하지 않습니다."));
+        Member.checkMember(loginId, member);
+        return member;
     }
 
     /**
      * 자가점검 결과 저장
      */
     @Transactional
-    public Member createSelfCheck(Long memberId, SelfCheckRequest request) {
+    public Member createSelfCheck(String loginId, Long memberId, SelfCheckRequest request) {
         Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
-
+                .orElseThrow(() -> new IllegalArgumentException("해당 회원이 존재하지 않습니다."));
+        Member.checkMember(loginId, member);
         member.updateBasicSelfCheck(request);
         return member;
     }
