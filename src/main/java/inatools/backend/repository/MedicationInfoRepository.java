6a455971b6c25722b997e293679817acb31ef029
Minkyu Shin <digi1k2001@gmail.com>
2024-08-14 02:30:20 +0900

    Fix: 건강 정보 전체 조회 시 비활성화된 복용약 정보로 조회되도록 수정

--- src/main/java/inatools/backend/repository/MedicationInfoRepository.java
+++ src/main/java/inatools/backend/repository/MedicationInfoRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicationInfoRepository extends JpaRepository<MedicationInfo, Long> {
 
+    List<MedicationInfo> findAllByMemberId(Long memberId);
+
     /**
      * active 상태인 복용약 정보 조회
      */
