2333b5167d5ea40bcc36a0dbfe9d8bc65fbd7ed2
Minkyu Shin <digi1k2001@gmail.com>
2024-08-15 14:32:16 +0900

    Chore: 가입 시 아이디 및 비밀번호 제한

--- src/main/java/inatools/backend/dto/member/LoginRequest.java
+++ src/main/java/inatools/backend/dto/member/LoginRequest.java
@@ -6,13 +6,11 @@ import jakarta.validation.constraints.Size;
 
 public record LoginRequest(
 
-        @Size(min = 5, max = 50)
-        @NotBlank(message = "아이디를 입력해주세요.")
+        @Size(min = 5, max = 20, message = "아이디는 5자 이상, 20자 이하여야 합니다.")
         @Schema(description = "아이디", example = "pil0602")
         String userId,
 
         @Size(min = 6, max = 12, message = "비밀번호는 6자 이상, 12자 이하여야 합니다.")
-        @NotBlank(message = "비밀번호를 입력해주세요.")
         @Schema(description = "비밀번호", example = "010602")
         String password
 ) {
--- src/main/java/inatools/backend/dto/member/SignUpRequest.java
+++ src/main/java/inatools/backend/dto/member/SignUpRequest.java
@@ -3,6 +3,8 @@ package inatools.backend.dto.member;
 import inatools.backend.domain.Member;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
@@ -12,15 +14,18 @@ public record SignUpRequest (
         @NotEmpty String username,
 
         @Schema(description = "아이디", example = "pil0602")
-        @NotEmpty String userId,
+        @Size(min = 5, max = 20, message = "아이디는 5자 이상, 20자 이하여야 합니다.")
+        String userId,
 
         @Schema(description = "비밀번호", example = "010602")
-        @NotEmpty String password,
+        @Size(min = 6, max = 12, message = "비밀번호는 6자 이상, 12자 이하여야 합니다.")
+        String password,
 
         @Schema(description = "이메일", example = "pil0602@naver.com")
         @NotEmpty String email,
 
         @Schema(description = "전화번호", example = "01012345678")
+        @Pattern(regexp = "\\d{10,11}", message = "전화번호는 10~11자리 숫자여야 합니다.")
         @NotEmpty String phone
 ) {
 
